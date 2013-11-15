4c59a96a0d0ee867ec5a58c9673798bc96351ecc
William Karsten <yme_ynot45@hotmail.com>
2013-11-15 13:18:29 -0600

    setting up the background

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,18 @@
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
-document.body.appendChild(canvas);
-
-
 $(document).ready(function() {
+
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  var bgReady = false;
+  var bgImage = new Image();
+
+  canvas.width = 512;
+  canvas.height = 480;
+  document.body.appendChild(canvas);
+
+  bgImage.onload = function () {
+    bgReady = true;
+  };
+  bgImage.src = "images/background.png";
+
 
 });
