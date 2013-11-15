50f7eb882f5678c14c88f50bae41f63147e9f3d4
William Karsten <yme_ynot45@hotmail.com>
2013-11-14 18:26:10 -0600

    adding the basic canvas dimensions

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,10 @@
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+
 $(document).ready(function() {
 
 });
